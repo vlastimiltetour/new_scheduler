aa0b56c699fe220f54e10b38c48e00250de7c13d
vlastimil.tetour <v.tetour@gmail.com>
2026-09-09 21:36:29 +0200

    dockerfile added

--- app/models/person.py
+++ app/models/person.py
@@ -1,10 +1,13 @@
 from typing import List, Optional  
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from app.models.entity import Entity
+from uuid import UUID, uuid4
+
 
 # Entity
 @dataclass
 class Person(Entity):
+   id: UUID = field(default_factory=uuid4)
    name: str = None
    email: str = None 
    person_type: str = None
